608e6b8cb0ccb3bc3d06de395f947908739663e4
Iván Uruchurtu <im@vheissu.me>
2017-04-23 14:28:55 -0500

    added browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,32 @@ var gulp  = require('gulp'),
     gutil = require('gulp-util');
     sass  = require('gulp-sass');
     sourcemaps = require('gulp-sourcemaps');
+    bs = require('browser-sync').create(); // create a browser sync instance.
 
 // process sass files to css files
-gulp.task('build-css', function() {
+gulp.task('sass', function() {
 	return gulp.src('src/scss/*.scss')
 	.pipe(sourcemaps.init())  // Process the original sources
 		.pipe(sass())
 	.pipe(sourcemaps.write()) // Add the map to modified source.
-	.pipe(gulp.dest('dist/assets/css'));
+	.pipe(gulp.dest('dist/assets/css'))
+	.pipe(bs.reload({stream: true}));
+});
+
+
+gulp.task('browser-sync', function() {
+	bs.init({
+		server: {
+			baseDir: "./"
+		}
+	});
 });
 
 // create watch task
-gulp.task('watch', function() {
-  gulp.watch('src/scss/**/*.scss', ['build-css']);
+gulp.task('watch', ['browser-sync'], function () {
+    gulp.watch("src/scss/*.scss", ['sass']);
+    gulp.watch("*.html").on('change', bs.reload);
 });
 
 // create a default task and just log a message
-gulp.task('default', ['build-css']);
+gulp.task('default', ['sass']);
